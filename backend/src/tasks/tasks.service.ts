3208385f67c0d03d1c0001153b65ef0444935ff6
sagiv bonali <sagivb13@gmail.com>
2023-04-12 18:41:50 +0300

    add Random ID for record

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,36 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTask } from 'src/dto/tasks/createTask.dto';
 import { UpdateTask } from 'src/dto/tasks/updateTask.dto';
+import { v4 as uuidv4 } from 'uuid';
 
-const tasks: Array<task> = [
-  {
-    id: '1',
-    title: 'Learn english',
-    createdAt: new Date(new Date().setFullYear(new Date().getFullYear() - 30)),
-    isCompleted: false,
-  },
-  {
-    id: '2',
-    title: 'Eat food',
-    createdAt: new Date(new Date().setMonth(new Date().getMonth() - 5)),
-    isCompleted: false,
-  },
-];
+const tasks: Array<task> = [];
+
 @Injectable()
 export class TasksService {
-  private createId() {
-    const id = (Math.random() * 100).toFixed(0);
-
-    if (tasks.findIndex((task) => task.id === id) >= 0) return this.createId();
-    return id;
-  }
-
   getTasks() {
     return tasks;
   }
 
   createTask(task: CreateTask) {
-    const id = this.createId();
+    const id = uuidv4();
     const newTask: task = {
       id,
       title: task.title,
