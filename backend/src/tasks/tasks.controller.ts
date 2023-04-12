2ba580a7c0a870de3d73b058b32c0823b9ac3325
sagiv bonali <sagivb13@gmail.com>
2023-04-12 10:38:48 +0300

    create getTasks service and controller

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  getTasks() {
+    return this.tasksService.getTasks();
+  }
+}
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+const tasks = [
+  { id: 1, title: 'Learn english' },
+  { id: 2, title: 'Eat food' },
+];
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  getTasks() {
+    return tasks;
+  }
+}
