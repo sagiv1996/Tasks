d207c37aebb37d5483d46ef13c5de7cc57450aeb
sagiv bonali <sagivb13@gmail.com>
2023-04-12 19:12:46 +0300

    DTO - create task with createdAt

--- backend/src/dto/tasks/createTask.dto.ts
+++ backend/src/dto/tasks/createTask.dto.ts
@@ -1,7 +1,10 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsDateString } from 'class-validator';
 
 export class CreateTask {
   @IsString()
   @IsNotEmpty()
   title: string;
+
+  @IsDateString()
+  createdAt: Date = new Date();
 }
