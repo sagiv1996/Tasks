ce0af262b31c213c529b41f0a053e69dc746b267
sagiv bonali <sagivb13@gmail.com>
2023-04-13 17:41:47 +0300

    Add backendApi variable

--- ui/src/instance.ts
+++ ui/src/instance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const backendApi = "http://localhost:3001/tasks";
 const instance = axios.create({
-  baseURL: "http://localhost:3001/tasks",
+  baseURL: backendApi,
 });
 
 export default instance;
